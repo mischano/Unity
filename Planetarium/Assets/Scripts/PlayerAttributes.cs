cd76a2044692bd45d81b574c4b10e2b045b4861a
m&m <43050166+mischano@users.noreply.github.com>
2021-10-29 16:19:38 -0700

    Glowing Hearts
    - Added methods to PlayerAttributes to make the hearts glow if the number of hearts
      is less than user given amount.

--- Planetarium/Assets/Scripts/PlayerAttributes.cs
+++ Planetarium/Assets/Scripts/PlayerAttributes.cs
@@ -17,6 +17,10 @@ public class Health
     // Max number of hearts the player can have. 
     [SerializeField, Range(1, 9)]
     public int maxNumberOfHearts = 5;
+    
+    // At this variable hearts will start glowing.
+    [SerializeField, Range(1, 3)]
+    public int glowHeartsAt = 2;
 
     public Image[] heartsList;
     public Sprite fullHeart;
@@ -48,10 +52,14 @@ public class PlayerAttributes : MonoBehaviour
     // Reference to oxygen class
     public Oxygen oxygen;
 
+    private bool isSpriteVisible;
+
     private void Awake()
     {
         //health.currentNumberOfHearts = 5;
         //health.maxNumberOfHearts = 5;
+
+        InvokeRepeating("GlowHealth", 0, 0.5f);
     }
 
     private void Update()
@@ -59,7 +67,7 @@ public class PlayerAttributes : MonoBehaviour
         HandleHealth();
         HandleOxygen();
     }
-    
+
     /* Handles the player health attributes */
     private void HandleHealth()
     {
@@ -124,6 +132,46 @@ public class PlayerAttributes : MonoBehaviour
         }
     }
     
+    /* Handles changing the color of heart sprites when 
+     * player's health is low */
+    private void GlowHealth()
+    {
+        // Return if the player doesn't have low health.
+        if (health.currentNumberOfHearts > health.glowHeartsAt)
+        {
+            // Return if the first heart sprite is visible
+            // *if the first sprite is visible, all others are visible.
+            if (health.heartsList[0].color.a == 255)
+            {
+                return;
+            }
+            // Make the sprites visible, then return.
+            else
+            {
+                Color a = health.heartsList[0].color;
+                a.a = 255;
+                for (int i = 0; i < health.currentNumberOfHearts; i++)
+                {
+                    health.heartsList[i].color = a;
+                }
+            }
+        }
+        
+        // Get the current heart sprite color.
+        Color currentColor = health.heartsList[0].color;
+
+        // Change the alpha channel of the sprite.
+        currentColor.a = isSpriteVisible ? 0 : 255;
+        
+        // For each red heart, set its color to currentColor.
+        for (int i = 0; i < health.currentNumberOfHearts; i++)
+        {
+            health.heartsList[i].color = currentColor;
+        }
+        
+        isSpriteVisible = !isSpriteVisible;
+    }
+
     /* Increases the number of hearts. 
      * Called from ConsumableBuff */
     public void AddHealth(int amount)
@@ -131,17 +179,17 @@ public class PlayerAttributes : MonoBehaviour
         health.currentNumberOfHearts += amount;
     }
 
+    /* Increases the number of oxygen tanks. 
+     * Called from ConsumableBuff */
+    public void AddOxygen(int amount) 
+    {
+        oxygen.currentNumberOfOxygen += amount;
+    }
+    
     /*decreases the number of hearts.
      Called from EnemyFollowPlayer*/
     public void TakeDamage(int amount)
     {
         health.currentNumberOfHearts -= amount;
     }
-
-    /* Increases the number of oxygen tanks. 
-     * Called from ConsumableBuff */
-    public void AddOxygen(int amount) 
-    {
-        oxygen.currentNumberOfOxygen += amount;
-    } 
 }
