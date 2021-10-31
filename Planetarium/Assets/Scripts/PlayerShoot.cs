24475ec6f4f94352abb0896eb2411ac86383b27e
John Yee <johnyee818@gmail.com>
2021-10-31 14:42:56 -0700

    Add crosshair, adjust camera, fire projectile based on cam

--- Planetarium/Assets/Scripts/PlayerShoot.cs
+++ Planetarium/Assets/Scripts/PlayerShoot.cs
@@ -9,13 +9,16 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    public Rigidbody projectile;
-    public Transform SpawnPoint;
-    public float speed;             // speed of projectile
+    [SerializeField] GameObject _projectile;
+    [SerializeField] Transform _spawnPoint;
+    [SerializeField] Transform _spawnPointParent;
+    [SerializeField] float _speed;             // speed of projectile
+
+    const float INFINITY = 100f;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetButtonDown("Fire1"))
         {
             FireProjectile();
         }
@@ -23,7 +26,29 @@ public class PlayerShoot : MonoBehaviour
 
     void FireProjectile()
     {
-        Rigidbody clone = (Rigidbody)Instantiate(projectile, SpawnPoint.position, projectile.rotation);
-        clone.velocity = SpawnPoint.TransformDirection(Vector3.forward * speed);
+        Ray crosshairRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
+        RaycastHit hit;
+        Vector3 targetPoint, direction;
+        if (Physics.Raycast(crosshairRay, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            // We aren't aiming at anything, so aim at a point far away.
+            targetPoint = Camera.main.transform.position + crosshairRay.direction * INFINITY;
+        }
+
+        direction = targetPoint - _spawnPointParent.position;
+
+        _spawnPointParent.rotation = Quaternion.LookRotation(direction);
+
+        GameObject clone = Instantiate(_projectile, _spawnPoint.position, Quaternion.identity);
+        clone.GetComponent<Rigidbody>().velocity = (targetPoint - _spawnPoint.position).normalized * _speed;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(_spawnPoint.position, 0.02f);
     }
 }
