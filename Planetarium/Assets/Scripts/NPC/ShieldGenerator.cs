9d83af8a7d02a06251795e9e59ccbe2060fb92e9
vohnjogel <vohnjogel@gmail.com>
2021-11-01 19:53:11 -0700

    created Shield Generator prefab with placeholder textures

--- Planetarium/Assets/Scripts/NPC/ShieldGenerator.cs
+++ Planetarium/Assets/Scripts/NPC/ShieldGenerator.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ShieldGenerator : MonoBehaviour
 {
+    [SerializeField] GameObject _shield;
+    [SerializeField] UnityEvent _onDeath;
+
     public void OnDeath()
     {
-        Destroy(gameObject, 0.1f);
+        Destroy(_shield);
+        Destroy(gameObject, 1.0f);
     }
 }
