ed7c41ae3d71619e3fd67f3f031b9c4c24b5e778
John Yee <johnyee818@gmail.com>
2021-11-03 19:46:23 -0700

    EnemyFollowPlayer: revert to ebcc19

--- Planetarium/Assets/Scripts/NPC/EnemyFollowPlayer.cs
+++ Planetarium/Assets/Scripts/NPC/EnemyFollowPlayer.cs
@@ -9,61 +9,56 @@ public class EnemyFollowPlayer : MonoBehaviour
     private GameObject _player;
     private Vector3 _playerPos;
     private _PlayerHealth _playerAttributes;
-    private Transform _planet;
+    private CustomGravityRigidbody _cgrb;
 
     // speed of the moving enemy
     public float speed = 1.5f;
-
     // enemy will follow the player if within a certain range
     public float followRadius = 20.0f;
 
     public float rotationSpeed = 300f;
-
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _player = GameObject.FindGameObjectWithTag("Player");
         _playerAttributes = _player.GetComponent<_PlayerHealth>();
-        _planet = GameObject.FindWithTag("Planet").transform;
+        _cgrb = GetComponent<CustomGravityRigidbody>();
     }
 
     // Update is called once per frame
-        void Update()
+    void Update()
+    {
+        _playerPos = _player.transform.position;
+        //Debug.Log(playerPos);
+        if ((_playerPos - _rb.position).magnitude <= followRadius)
         {
-            _playerPos = _player.transform.position;
-            //Debug.Log(playerPos);
-            if ((_playerPos - _rb.position).magnitude <= followRadius)
-            {
-                _rb.constraints = RigidbodyConstraints.None;
-                followPlayer();
-            }
-            else if ((_playerPos - _rb.position).magnitude > followRadius)
-            {
-                _rb.constraints = RigidbodyConstraints.FreezeAll;
-            }
+            _rb.constraints = RigidbodyConstraints.None;
+            followPlayer();
         }
+        else if ((_playerPos - _rb.position).magnitude > followRadius)
+        {
+            _rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
 
-        void followPlayer()
-        {
-            // enemy will face the player
-            transform.LookAt(_player.transform, Vector3.up);
-            // move enemy toward the player
-            Vector3 moveDir = Vector3.MoveTowards(_rb.position, _playerPos, speed * Time.deltaTime);
-            _rb.MovePosition(moveDir);
-            Vector3 gravityUp = (transform.position - _planet.position).normalized;
-            Quaternion targetRotation = Quaternion.LookRotation(moveDir, _planet.up);
-            transform.rotation =
-                Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
-            transform.rotation = Quaternion.FromToRotation(transform.up, gravityUp) * transform.rotation;
-        }
+    void followPlayer()
+    {
+        // enemy will face the player
+        transform.LookAt(_player.transform, Vector3.up);
+        // move enemy toward the player
+        Vector3 moveDir = Vector3.MoveTowards(_rb.position, _playerPos, speed * Time.deltaTime);
+        _rb.MovePosition(moveDir);
 
-        void OnCollisionEnter(Collision other)
+        Quaternion targetRotation = Quaternion.LookRotation(moveDir, _cgrb.upAxis);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (other.gameObject.CompareTag("Player"))
-            {
-                _playerAttributes.TakeDamage(1);
-            }
+            _playerAttributes.TakeDamage(1);
         }
-    
+    }
 }
