c231cb0b738bd4366b3b1657b659c947771b99f3
vohnjogel <vohnjogel@gmail.com>
2021-11-08 11:18:08 -0800

    added shield polish (bubble shield)
    Not quite right yet, need better visibility through shield

---
+++ Planetarium/Assets/Scripts/Camera/LookAtCamera.cs
@@ -0,0 +1,22 @@
+// code from https://www.youtube.com/watch?v=jdAbVkre8cw
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    Camera _cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = _cam.transform.position - transform.position;
+    }
+}
