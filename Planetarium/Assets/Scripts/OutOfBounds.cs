950521c15fc93f5502b310a55aab6f73d8f7502c
ashmayer <asmayer@calpoly.edu>
2021-11-18 10:44:48 -0800

    OutOfBoundsScript

---
+++ Planetarium/Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private GameManager _gm;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = FindObjectOfType<GameManager>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.SetPositionAndRotation(_gm.lastCheckpoint, _gm.lastCheckpointRotation);
+        }
+    }
+    }
+}
