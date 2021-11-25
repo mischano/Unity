005b707a9d9a94f847713263da3aa4a43e9c2d98
ashmayer <asmayer@calpoly.edu>
2021-11-24 22:45:24 -0600

    CheckpointsWithRespawnSparkEffect

--- Planetarium/Assets/Scripts/OutOfBounds.cs
+++ Planetarium/Assets/Scripts/OutOfBounds.cs
@@ -2,21 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Layouts;
 
 public class OutOfBounds : MonoBehaviour
 {
     private GameManager _gm;
+    [SerializeField] private ParticleSystem respawnParticle;
     
     // Start is called before the first frame update
     void Start()
     {
         _gm = FindObjectOfType<GameManager>();
+        
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        // repositions character and Plays particle effect
         if (other.gameObject.CompareTag("Player"))
         {
+            respawnParticle.Play();
             other.transform.SetPositionAndRotation(_gm.lastCheckpoint, _gm.lastCheckpointRotation);
         }
     }
